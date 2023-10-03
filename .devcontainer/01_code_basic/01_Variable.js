2e6764965de867a0e3da6c0982a48b43efea7a60
Akash <89128410+akvs00001@users.noreply.github.com>
2023-10-03 17:14:09 +0000

    container config done

---
+++ .devcontainer/01_code_basic/01_Variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "akvs00001@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhagalpur"
+
+accountId = 2 
+accountEmail = "ttheakashsaha@gmail.com"
+accountPassword = "2345"
+accountCity = "Ahmedabad"
+
+
+// console.log(accountId);// for single  output at 1 time
+console.table([accountId,accountEmail,accountPassword,accountCity]);// table for multipal output at same time
