015be7ae7ce9a852aee58b1e7bcd86f16f4b39a8
Akash <89128410+akvs00001@users.noreply.github.com>
2023-10-06 23:31:05 +0530

    Update 01_variable.js

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -2,10 +2,11 @@ const accountId = 144553
 let accountEmail = "akvs00001@gmail.com"
 var accountPassword = "12345"
 accountCity = "Bhagalpur"
+let accountState;
 
 accountEmail = "ttheakashsaha@gmail.com"
 accountPassword = "2345"
 accountCity = "Ahmedabad"
 
-console.table([accountId, accountEmail, accountPassword, accountcity])
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
 
