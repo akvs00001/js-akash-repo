8cf3b2507425e19bcb0d3a040c2200cdc43168db
Akash <89128410+akvs00001@users.noreply.github.com>
2023-10-07 08:46:51 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not Browser
+
+// console.log(3+3) console.lo("akash") //cause an earror can be correct by putting ; afet first console 
+//console.log(3+3); console.log("akash") // but this is not good consern as code readiblity
+//console.log (3
+//   +3) // code readiblity should be high
+
+console.log(3+3)
+                       // that looks good as readiblity of code
+console.log("Akash")    
+
+//documentations tc39.es and mdn keep in touch
+// datatypes
+
+let name = "akash"
+let age = 18
+let isloggedIn = false 
+
+
+// number => 2 to power 53
+// bigint 
+// string => " " or ' '
+// boolean => true/false
+// null =>  Standalone value
+// undefined =>
+// symbol => unique
+
+// Object 
+
+console.log(typeof "akash")      // string // datatypes 
+console.log(typeof age)         // number
+console.log(typeof isloggedIn) // boolean
+console.log(typeof null)      // object
+console.log(typeof undefined)// undefined
+
+// see the documentations of ecma specification
+
